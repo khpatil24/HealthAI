26f1749f64bc6dc96fe3657d5b1db35bf23b7604
khpatil24 <116716070+khpatil24@users.noreply.github.com>
2025-02-23 07:42:53 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import pickle
-from main import model, process_user_input  # Ensure 'model' and 'process_user_input' exist in main.py
+from main import model, process_user_input 
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)   
 
 model_path = 'model.pkl'
 with open(model_path, 'rb') as f:
